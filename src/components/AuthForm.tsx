db8254ab02882ee58d9953146831084d44fb2416
Muhammad-Saifullah50 <saifullahm2005@gmail.com>
2023-11-29 22:36:20 +0500

    connect mongodb

--- src/components/AuthForm.tsx
+++ src/components/AuthForm.tsx
@@ -3,6 +3,9 @@
 import { useCallback, useState } from "react"
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form"
 import Input from "./Input"
+import Button from "./Button"
+import AuthSocialButton from "./AuthSocialButton"
+import { BsGithub, BsGoogle } from 'react-icons/bs'
 
 type Variant = 'LOGIN' | 'REGISTER'
 
@@ -42,23 +45,89 @@ const AuthForm = () => {
       // nextauth signin
     }
 
-    const socialAction = (action: string) => {
-      setIsLoading(true)
 
-      //nextauth social signin
-    }
+  }
+  const socialAction = (action: string) => {
+    setIsLoading(true)
+
+    //nextauth social signin
   }
   return (
     <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
       <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
         <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
-          <Input 
-          id='email'
-          label="Email"
-          register={register}
-          errors={errors}
+
+          {variant === 'REGISTER' && (
+            <Input
+              id='name'
+              label="Name"
+              register={register}
+              errors={errors}
+              disabled={isLoading}
+            />
+          )}
+          <Input
+            id='email'
+            label="Email Address"
+            type="email"
+            register={register}
+            errors={errors}
+            disabled={isLoading}
+
           />
+          <Input
+            id='password'
+            label="Passowrd"
+            type="password"
+            register={register}
+            errors={errors}
+            disabled={isLoading}
+
+          />
+
+          <div>
+            <Button
+              disabled={isLoading}
+              fullWidth
+              type="submit"
+            >
+              {variant === 'LOGIN' ? 'Sign in' : 'Register'}</Button>
+          </div>
         </form>
+
+        <div className="mt-6">
+          <div className="relative">
+            <div className="absolute inset-0 flex items-center">
+              <div className="w-full border-t border-gray-300" />
+
+
+            </div>
+            <div className="relative flex justify-center text-sm">
+              <span className="bg-white px-2 text-gray-900">
+                Or continue with
+              </span>
+            </div>
+          </div>
+
+          <div className="mt-6 flex gap-2">
+            <AuthSocialButton
+              icon={BsGithub}
+              onClick={() => socialAction('github')}
+            />
+            <AuthSocialButton
+              icon={BsGoogle}
+              onClick={() => socialAction('google')}
+            />
+          </div>
+        </div>
+
+        <div className="flex justify-center gap-2 text-sm mt-6 px-2 text-gray-500">
+          {variant === 'LOGIN' ? 'New to Chatteese' : 'Already have an account?'}
+
+          <div className="underline cursor-pointer" onClick={toggleVariant}>
+{variant === 'LOGIN' ? 'Create an account' : 'Log in'}
+          </div>
+        </div>
       </div>
     </div>
   )
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -24,9 +24,9 @@ const Input = ({ label, id, type, required, register, errors, disabled }: InputP
                     disabled={disabled}
                     autoComplete={id}
                     {...register(id, { required })}
-                    className={clsx("form-input w-full rounded-md block border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6", 
-                    errors[id] && "focus:ring-rose-500",
-                    disabled && "opacity-50 cursor-default"
+                    className={clsx("form-input w-full rounded-md block border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6",
+                        errors[id] && "focus:ring-rose-500",
+                        disabled && "opacity-50 cursor-default"
                     )}
                 />
             </div>
