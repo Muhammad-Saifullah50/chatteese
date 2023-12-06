ad74483d4c7e666f10fda1e81e8bfeae70a5c8fe
Muhammad-Saifullah50 <saifullahm2005@gmail.com>
2023-12-06 08:11:39 +0500

    corrected middleware

--- src/lib/authOptions.ts
+++ src/lib/authOptions.ts
@@ -55,6 +55,9 @@ export const authOptions: AuthOptions = {
     session: {
         strategy: 'jwt'
     },
+    pages:{
+        signIn: '/'
+    },
 
     secret: process.env.NEXTAUTH_SECRET,
 
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,18 +1,35 @@
-import { withAuth } from "next-auth/middleware";
+import { NextRequest, NextResponse } from "next/server";
 
+export const middleware = async (request: NextRequest) => {
 
+    let pathname = request.nextUrl.pathname
+    
+    let token = request.cookies.get('next-auth.session-token') || request.cookies.get('__Secure-next-auth.session-token')
 
+    const publicPath = pathname === '/';
 
-export default withAuth({
-    pages: {
-        signIn: '/'
+    if (!publicPath && !token) {
+        const response = NextResponse.redirect(new URL('/', request.nextUrl))
+        return response
+
     }
-})
+    if (token && pathname === '/') {
+        const response = NextResponse.redirect(new URL('/users', request.nextUrl))
+        return response
+    }
+    if (token) {
+        const response = NextResponse.next()
+        return response
+    }
+}
 
-export const config = {
+
+
+export const config =
+{
     matcher: [
-        
+        '/',
         '/users/:path*',
-        '/conversations/:path*'
+        '/conversations/:path*',
     ]
-};
+}
